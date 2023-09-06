0386326ceaba2b15ff98c999897a9047cf782c94
adam.legradi <adam.legradi@ans.co.uk>
2023-09-06 11:26:41 +0100

    Commented out debug printing

--- data.py
+++ data.py
@@ -8,11 +8,11 @@ def get_question_data():
     global question_data
     setup = SetupInterface()
     parameters = setup.parameters
-    print(f"Parameters: {parameters}")  # Debug info
+    # print(f"Parameters: {parameters}")  # Debug info
 
     response = requests.get("https://opentdb.com/api.php", params=parameters)
     response.raise_for_status()
     data = response.json()
 
     question_data = data["results"]
-    print(f"Question data: {question_data}")  # Debug info
+    # print(f"Question data: {question_data}")  # Debug info
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from ui import QuizInterface
 
 # Start setup screen and get question data
 data.get_question_data()
-# print(data.question_data)
 
 question_bank = []
 for question in data.question_data:
@@ -15,7 +14,7 @@ for question in data.question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(f"Question_bank: {question_bank}")  # Debug info
+# print(f"Question_bank: {question_bank}")  # Debug info
 
 quiz = QuizBrain(question_bank)
 quiz_ui = QuizInterface(quiz)
